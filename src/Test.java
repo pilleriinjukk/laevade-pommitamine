60f84aceab88561cd91b361199a10808173e9afb
unknown <triinmirjamtark@gmail.com>
2019-03-31 21:06:44 +0300

    Mänguväljad ja kasutajaga suhtlemine.

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,23 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 public class Test {
     public static void main(String[] args) {
-        //ArrayList<ArrayList<String>> list = {{"-", "-"}};
+
+        JOptionPane.showMessageDialog( null, "Tere" );
+        //System.exit(0);
+
+        String sisestatakse =
+        JOptionPane.showInputDialog(
+                "Sisesta koordinaat, mida soovid pommitada (näiteks A3)");
+
+        System.out.println(sisestatakse);
+
         String[][] list = {{"x", "-", "-", "-"}, {"x", "-", "o", "-"}, {"o", "o","-","o"}, {"x", "-", "o", "-"}};
 
         for (String[] el : list) {
-            //System.out.println(Arrays.toString(el));
             for (String märk : el) {
                 System.out.print(märk);
             }
